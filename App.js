53c21dede8544bd925cec0d9259605ef621bc327
Shivam Gupta <100027945+shivamcool12345@users.noreply.github.com>
2022-05-26 23:06:34 +0530

    todo use-effect

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Todo }from './components/Todo';
+//import  { useEffect } from 'react';
+function App() {
+  return (
+    <div className="App">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
